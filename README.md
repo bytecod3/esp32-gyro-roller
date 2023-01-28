54a5be1219369cb72efb08d10dda5c42d0b2b5e0
bytecod31 <emwiti658@gmail.com>
2023-01-28 03:12:00 +0300

    added target draw function

---
+++ README.md
@@ -0,0 +1,1 @@
+Implement a rolling platform using MPU6050 on a SSD1306 OLED screen using ESP32 IDF
--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,40 @@
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT);
 
-void _display_settings(){
+void _set_display_settings(){
+  /*
+  Set display parameters
+  */
   display.setTextSize(1);
   display.setTextColor(WHITE);
 
+  // Clear the buffer.
+  display.clearDisplay();
+
 }
 
-void _draw_target_circle(){
-  display.setCursor();
-  display.drawCircle();
+void _draw_target_point_circle(){
+  /*
+  Draw target like circle on the screen
+  */
+  uint8_t _target_point_radius = 10;
+  uint8_t _target_point_x_offset = (SCREEN_WIDTH/2);
+  uint8_t _y_offset = 10;
+  uint8_t _target_point_line_marker_height = 6;
+  uint8_t _center_circle_radius = 2;
+  
+  // draw target outer circle
+  display.drawCircle(_target_point_x_offset, _y_offset+_target_point_radius, _target_point_radius+_target_point_line_marker_height/2, WHITE);
+
+  // lines around the circle
+  display.drawFastVLine(SCREEN_WIDTH/2, _target_point_radius-_target_point_line_marker_height, _target_point_line_marker_height, WHITE); // vertical top
+  display.drawFastVLine(SCREEN_WIDTH/2, (_y_offset + _target_point_radius*2), _target_point_line_marker_height, WHITE); // vertical bottom
+  display.drawFastHLine((SCREEN_WIDTH/2) - _target_point_radius-_target_point_line_marker_height, (_y_offset + _target_point_radius), _target_point_line_marker_height, WHITE ); // horizontal left
+  display.drawFastHLine((SCREEN_WIDTH/2) + _target_point_radius, (_y_offset + _target_point_radius), _target_point_line_marker_height, WHITE ); // horizontal right
+
+  // draw center point circle
+  display.drawCircle(SCREEN_WIDTH/2, _y_offset + _target_point_radius, _center_circle_radius, WHITE);
+    
 }
 
 void setup(){               
@@ -25,13 +50,8 @@ void setup(){
     for (;;); // loop forever
   }
 
-  _display_settings();
-
-  // Clear the buffer.
-  display.clearDisplay();
-
-  display.setCursor(0, 28);
-  display.println("ESP32 GYRO");
+  _set_display_settings();
+  _draw_target_point_circle();
 
   display.display();
 
